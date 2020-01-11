e05f48cbc8a583bf0d74e0f25a80f515bceebcc9
Jason Tavares <jaytavares@gmail.com>
2020-01-11 13:46:22 -0500

    Add ir code

--- AudioSense.ino
+++ AudioSense.ino
@@ -19,6 +19,72 @@ void setup() {
     pinMode(13, OUTPUT); // TESTING
 }
 
+/*
+ * IR STUFF -- Move to separate file
+ */
+
+// pulse parameters in usec
+#define NEC_HDR_MARK  9000
+#define NEC_HDR_SPACE 4500
+#define NEC_BIT_MARK  560
+#define NEC_ONE_SPACE 1600
+#define NEC_ZERO_SPACE  560
+
+#define TOPBIT 0x80000000
+
+void mark(int time) {
+    digitalWrite(IR_LINE, LOW);
+    delayMicroseconds(time);
+}
+
+void space(int time) {
+    digitalWrite(IR_LINE, HIGH);
+    delayMicroseconds(time);
+}
+
+void sendNEC(unsigned long data) {
+    pinMode(IR_LINE, OUTPUT);
+    digitalWrite(IR_LINE, HIGH);
+    delay(100);
+
+    mark(NEC_HDR_MARK);
+    space(NEC_HDR_SPACE);
+
+    for (int i = 0; i < 32; i++) {
+        if (data & TOPBIT) {
+            mark(NEC_BIT_MARK);
+            space(NEC_ONE_SPACE);
+        }
+        else {
+            mark(NEC_BIT_MARK);
+            space(NEC_ZERO_SPACE);
+        }
+        data <<= (uint8_t) 1;
+    }
+    mark(NEC_BIT_MARK);
+    space(0);
+
+    pinMode(IR_LINE, INPUT);
+}
+
+/*
+ * END IR STUFF
+ */
+
+bool lastPowerState = false;
+
+bool isPoweredOn() {
+    bool value = digitalRead(CONTROL_IN) == HIGH;
+
+    if (lastPowerState != value) {
+        Serial.print("Power: ");
+        Serial.println(value ? "ON" : "OFF");
+    }
+
+    lastPowerState = value;
+    return value;
+}
+
 bool lastSignalState = false;
 
 bool checkSignal() {
@@ -60,26 +126,14 @@ bool checkSignal() {
     return SIG_SENSED;
 }
 
-bool lastPowerState = false;
-
-bool isPoweredOn() {
-    bool value = digitalRead(CONTROL_IN) == HIGH;
-
-    if (lastPowerState != value) {
-        Serial.print("Power: ");
-        Serial.println(value ? "ON" : "OFF");
-    }
-
-    lastPowerState = value;
-    return value;
-}
 
 void loop() {
 
     // 1. Check CONTROL LINE IN from sub woofer, if HIGH speakers are already on, abort.
     isPoweredOn();
 
-    checkSignal();
+    if (checkSignal() && !isPoweredOn())
+        sendNEC(0x5D0532CD);
 
     // Match LED to system state
     // TODO: Handle CONTROL_IN floating state (used to flash LED when remote is used and when speakers are muted)
