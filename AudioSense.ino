386319dfbef4e84db44394209eaf8d24b7810baf
Jason Tavares <jaytavares@gmail.com>
2020-01-10 23:43:40 -0500

    Fix audio signal sensing

--- AudioSense.ino
+++ AudioSense.ino
@@ -2,53 +2,86 @@
 #include "User_Setup.h"
 
 // Define pins
-#define AUDIO_SENSE_PIN 4
+#define AUDIO_SENSE_PIN A0
 #define CONTROL_IN 5
 #define CONTROL_OUT 6
-#define IR_LINE
+#define IR_LINE 7
 
 // Define settings
-#define AUDIO_THRESHOLD 250  // milliseconds
+#define SIGNAL_THRESHOLD 250  // milliseconds
 
 void setup() {
+    Serial.begin(115200);
     pinMode(AUDIO_SENSE_PIN, INPUT);
     pinMode(CONTROL_IN, INPUT);
-    pinMode(CONTROL_OUT, LOW);
+    pinMode(CONTROL_OUT, OUTPUT);
     pinMode(IR_LINE, INPUT);
     pinMode(13, OUTPUT); // TESTING
 }
 
-// TESTING
-bool LED_ON = false;
+bool lastSignalState = false;
 
-void checkSignal() {
+bool checkSignal() {
     bool SIG_SENSED = false;
+    
+    // Find out if speaker is off
+    bool POWER_OFF = !digitalRead(CONTROL_IN);
 
-//    1. Check CONTROL LINE IN from sub woofer, if HIGH abort
-    if (digitalRead(CONTROL_IN) == LOW) {
-        // 2. Float the CONTROL LINE OUT (set to input) or set to 700mv if that doesn’t work
+    // If speaker is off, float the CONTROL_OUT line.
+    // This will turn on the optical input but won't turn on the power LED.
+    if (POWER_OFF)
         pinMode(CONTROL_OUT, INPUT);
 
-        unsigned long startTime = millis();
-        while (millis() - startTime < AUDIO_THRESHOLD) {
-            // 3. Check for signal on audio signal line
-            if (digitalRead(AUDIO_SENSE_PIN) == HIGH) {
-                SIG_SENSED = true;
-            }
+    // Check for audio signal
+    unsigned long startTime = millis();
+    while (millis() - startTime < SIGNAL_THRESHOLD) {
+        // I use analogRead instead of digitalRead in order to allow detection of the 3.3v audio signal
+        // even if the micro controller has a 5v logic level.
+        if (analogRead(AUDIO_SENSE_PIN) != 0) {
+            // A signal was sensed for longer than the threshold setting
+            SIG_SENSED = true;
         }
-        // 4. Reconnect CONTROL LINE
-        pinMode(CONTROL_OUT, OUTPUT);
-        digitalWrite(CONTROL_OUT, LOW);
     }
+
+    if (POWER_OFF){
+        // Reconnect CONTROL LINE
+        pinMode(CONTROL_OUT, OUTPUT);
+    }
+
+    // DEBUGGING
+    if (SIG_SENSED != lastSignalState) {
+        Serial.print("Audio signal: ");
+        Serial.println(SIG_SENSED);
+        digitalWrite(13, SIG_SENSED);
+    }
+
+    lastSignalState = SIG_SENSED;
+
+    return SIG_SENSED;
+}
+
+bool lastPowerState = false;
+
+bool isPoweredOn() {
+    bool value = digitalRead(CONTROL_IN) == HIGH;
+
+    if (lastPowerState != value) {
+        Serial.print("Power: ");
+        Serial.println(value ? "ON" : "OFF");
+    }
+
+    lastPowerState = value;
+    return value;
 }
 
 void loop() {
+
+    // 1. Check CONTROL LINE IN from sub woofer, if HIGH speakers are already on, abort.
+    isPoweredOn();
+
     checkSignal();
 
-    if (LED_ON) // TESTING
-        digitalWrite(13, HIGH);
-    else
-        digitalWrite(13, LOW);
+    digitalWrite(CONTROL_OUT, digitalRead(CONTROL_IN));
 
     delay(1000);
 }
